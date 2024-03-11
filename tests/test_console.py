dd7b9a3b207ccd60b4339cfdb3c0cdd5ac3e1624
Neutreing <preciousoloye@gmail.com>
2024-03-11 17:39:07 +0100

    updated tests folder

--- tests/test_console.py
+++ tests/test_console.py
@@ -5,12 +5,22 @@ from console import Console
 from unittest.mock import patch
 
 class TestConsole(unittest.TestCase):
+    """This is the unittest for the console file"""
 
     def test_quit_command(self):
         with patch('builtins.print') as mock_print:
+            #test the code here
             console = Console()
             result = console.onecmd('EOF')
             self.assertTrue(result)
+
+    def test_emptyline(self):
+        with patch('builtins.print') as mock_print:
+            console = Console()
+            result = console.emptyline()
+            self.assertIsNone(result)
+            mock_print.assert_not_called()
+
 
 if __name__ == "__main__":
     unittest.main()
