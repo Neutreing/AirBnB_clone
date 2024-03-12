9845bbfc1309358b10a9bacfd90308fb3afb0665
Neutreing <preciousoloye@gmail.com>
2024-03-12 18:44:13 +0100

    made my user profile stuff

---
+++ models/entities.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class User:
+    """Class representing a user profile."""
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
