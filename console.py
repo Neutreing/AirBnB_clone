52acdcf074a76342978eb0c0fb90d17eca02f49e
Neutreing <preciousoloye@gmail.com>
2024-03-19 10:55:58 +0100

    user profile part 1

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import cmd
+from models.entities import User
 
 
 class Console(cmd.Cmd):
@@ -20,11 +21,27 @@ class Console(cmd.Cmd):
         """This command exits the interactive shell."""
         return True
 
+    def register_create_profile_command(self):
+        """Registers the CreateProfileCommand."""
+        self.add_command("create_profiles", CreateProfileCommand)
+
+    def do_create_profile(self, args):
+        """Creats a new user profile"""
+        name = input("Enter your name: ")
+        email = input("Enter your email: ")
+        password = input("Enter your password: ")
+
+        new_user = User(name, email, password)
+        print("User profile created successfully: ")
+        print(f"Name: {new_user.name}")
+        print(f"Email: {new_user.email}")
+
 
 if __name__ == "__main__":
     import sys
     if len(sys.argv) > 1:
         Console().onecmd(' '.join(sys.argv[1:]))
     else:
-        Console().cmdloop()
+        console = Console()
+        console.cmdloop()
     print()
