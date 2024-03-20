612e46f8c9bec96bb04a0246e676fee45f6ddb1c
Neutreing <preciousoloye@gmail.com>
2024-03-20 14:34:07 +0100

    base model update

--- models/base_model.py
+++ models/base_model.py
@@ -15,5 +15,17 @@ class BaseModel:
 
     def __str__(self):
         """Method to represent an object as a string."""
-        return "[{}] ({}) {}".format)self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Method to update the 'updated_at' attribute with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Method to return a dictionary representation of an object"""
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = self.__class__.__name__
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.created_at.isoformat()
+        return dict_copy
 
