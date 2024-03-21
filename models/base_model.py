05b18973b8bc9c173b5ef982ccce14ad95b12d39
Neutreing <preciousoloye@gmail.com>
2024-03-21 10:29:10 +0100

    mistakes corrected in base model

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,22 @@ from datetime import datetime
 class BaseModel:
     """This defines all the common attributes/models for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize the BaseModel."""
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    try:
+                        setattr(self, key, datetime.syrptime(value, tform))
+                    except ValueError:
+                        print(f"Invalid datetime format for {key}: {value}")
+                    else:
+                        setattr(self, key, value)
 
     def __str__(self):
         """Method to represent an object as a string."""
