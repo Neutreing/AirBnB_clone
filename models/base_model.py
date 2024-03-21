b4d98668ec0e83326d474a7ac4273297d665d470
Neutreing <preciousoloye@gmail.com>
2024-03-21 10:34:41 +0100

    base model update

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 """Defines the BaseModel class."""
+import models
 import uuid
 from datetime import datetime
 
@@ -8,21 +9,24 @@ class BaseModel:
     """This defines all the common attributes/models for other classes"""
 
     def __init__(self, *args, **kwargs):
-        """Initialize the BaseModel."""
+        """Initialize the BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
         tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    try:
-                        setattr(self, key, datetime.syrptime(value, tform))
-                    except ValueError:
-                        print(f"Invalid datetime format for {key}: {value}")
-                    else:
-                        setattr(self, key, value)
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        # else:
+            # models.storage.new(self)
 
     def __str__(self):
         """Method to represent an object as a string."""
